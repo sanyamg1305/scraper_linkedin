f7506ded8bd8e928eea78f2746a80428ba8e8c68
sanyamg1305 <sanyam.raj.golechha@gmail.com>
2025-06-16 15:24:45 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Install Chromedriver compatible with Chrome
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+' | head -1)
+CHROMEDRIVER_VERSION=$(wget -qO- "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json" | grep -oP '"version": "'$CHROME_VERSION'[^"]*' | head -1 | cut -d'"' -f4)
+
+wget -O chromedriver_linux64.zip "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip"
+unzip chromedriver_linux64.zip
+mv chromedriver-linux64/chromedriver /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
+
+# Start Streamlit app
+streamlit run app.py --server.port=10000 --server.address=0.0.0.0
